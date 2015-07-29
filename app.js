f4b009fdcf0f61131d967ae0f67667234be8d6d3
Michael Harrison <mikey99222@yahoo.com>
2015-07-29 15:58:45 +0100

    Added stuf
    stuuuuuf

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var appid = "oM6g5QU41NKGl2PGlqxhQxV51WEYqPkj2EkuDXdJ";
 var apikey = "ma2Lgv3C4DjDUgNOv12ghmd82Ou60ZQpGsr3ubPx";
 var javakey = "CLw7NNxumkyJBvOJ2h2LJFJyq4nZZb8FnV8RFAIt";
+
+
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
+
+
 $(function() {
 	var file;
 	Parse.initialize(appid, javakey);
@@ -39,6 +52,58 @@ $(function() {
 	});
 
 
+	
+	var findclothes = new Parse.Query(Clothing);
+	findclothes.find({
+		success: function(results) {
+			results.forEach(function(result) {
+				if (result.attributes.type === 'Head') {
+					$("#cycle-1").cycle('add', '<img src=' + result.attributes.picture +
+						' width=100 height=200 />');
+				}
+				if (result.attributes.type === 'Torso') {
+					$("#cycle-2").cycle('add', '<img src=' + result.attributes.picture +
+						' width=500 height=200 />');
+				}
+				if (result.attributes.type === 'Legs') {
+					$("#cycle-3").cycle('add', '<img src=' + result.attributes.picture +
+						' />');
+				}
+				if (result.attributes.type === 'Feet') {
+					$("#cycle-4").cycle('add', '<img src=' + result.attributes.picture +
+						' width=500 height=150 />');
+				}
+			});
+$("#cycle-1").cycle();
+
+var head = getParameterByName('head') || 0;
+$("#cycle-1").cycle('goto', head);
+
+
+
+$("#cycle-2").cycle();
+var body = getParameterByName('body')|| 0;
+$("#cycle-2").cycle('goto', body);
+
+$("#cycle-3").cycle();
+
+var legs = getParameterByName('legs')|| 0;
+$("#cycle-3").cycle('goto', legs);
+
+
+$("#cycle-4").cycle();
+
+var feet = getParameterByName('feet')|| 0;
+$("#cycle-4").cycle('goto', feet);
+
+
+	}
+	});	
+	
+	
+	
+	
+	
 	// This function is called when the user clicks on Upload to Parse. It will create the REST API request to upload this image to Parse.
 	$('#uploadbutton').click(function() {
 		var serverUrl = 'https://api.parse.com/1/files/' + file.name;
